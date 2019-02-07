0eb380743d915a49077365487b0e84fa327a6392
isycat🐰 <ellaclqt@gmail.com>
2019-02-07 03:24:21 +0200

    Reticulate spleens

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+cmd=""
+if [ -f "pom.xml" ]; then
+if [ -e "$1" ]; then
+cmd="cmd.exe /c mvn $1";
+else
+cmd="cmd.exe /c mvn clean install";
+fi
+elif [ -f "package.json" ]; then
+if [ -e "$1" ]; then
+cmd="npm $1";
+else
+cmd="npm start";
+fi
+else
+echo "Build system not detected in directory";
+exit 1;
+fi
+
+$cmd
