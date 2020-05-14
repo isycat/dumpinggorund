415ecb7d5f49fa265e4da6c62a7b9de1f287e4a7
Isabella de Andrade 🐰 <isycat@users.noreply.github.com>
2020-05-14 14:19:41 +0200

    Compress fish files

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
-
 cmd=""
 if [ -f "pom.xml" ]; then
-if [ ! -z "$1" ]; then
-cmd="cmd.exe /c mvn $1";
-else
-cmd="cmd.exe /c mvn clean install";
-fi
+    if [ ! -z "$1" ]; then
+        cmd="cmd.exe /c mvn $1"
+    else
+        cmd="cmd.exe /c mvn clean install"
+    fi
 elif [ -f "package.json" ]; then
-if [ ! -z "$1" ]; then
-cmd="npm $1";
+    if [ ! -z "$1" ]; then
+        cmd="npm $1"
+    else
+        cmd="npm start"
+    fi
 else
-cmd="npm start";
-fi
-else
-echo "Build system not detected in directory";
-exit 1;
+    echo "Build system not detected in directory"
+    exit 1
 fi
 
 $cmd
